5f1aa61fe87578235e11c1fe5a3f2eec1ce23c58
vathevitechguy <aromosevictor@gmail.com>
2024-03-25 11:36:05 -0400

    . . .

--- src/landing/components/Footer.jsx
+++ src/landing/components/Footer.jsx
@@ -50,7 +50,7 @@ const Footer = () => {
         className={`flex  justify-between items-center md:flex-row flex-col w-full pt-6  border-t-[1px] border-t-[#3F3E45]`}
       >
         <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white ">
-          Copyright &copy; {`${new Date().getFullYear()}`} HooBank. All Rights
+          Copyright &copy; {`${new Date().getFullYear()}`} TrackFin. All Rights
           Reserved.
         </p>
         <div className="flex flex-row md:mt-0 mt-6">
